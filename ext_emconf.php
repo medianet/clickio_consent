e769b787947340497f7af7034a66a078c1a6d63f
Sabay <asabay@gmail.com>
2024-11-02 22:27:09 +0300

    -

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,5 +25,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Alexander Sabaev',
     'author_email' => 'asabay@gmail.com',
     'author_company' => 'Clickio',
-    'version' => '1.0.3',
+    'version' => '1.0.5',
 ];
