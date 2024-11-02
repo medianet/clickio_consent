f76b8a16293bcabaabc46c37d3e17549a76be5f2
Sabay <asabay@gmail.com>
2024-11-02 20:01:05 +0300

    -

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,1 +1,20 @@
- 
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Clickio consent',
+    'description' => 'Extension description',
+    'category' => 'plugin',
+    'author' => 'Alexander sabaev',
+    'author_email' => 'asabay@gmail.com',
+    'author_company' => 'Clickio',
+    'state' => 'stable',
+    'version' => '1.0.0-dev',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.4.0-12.3.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
