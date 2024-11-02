c3345c260346bdb18b009f06d2ad7c7e6495b943
Sabay <asabay@gmail.com>
2024-11-02 22:23:54 +0300

    -

---
+++ Classes/Controller/ConfigController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Clickio\ClickioConsent\Controller;
+
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+class ConfigController extends ActionController
+{
+    public function indexAction()
+    {
+        // ѕередаем нужные параметры в шаблон, если это нужно
+    }
+}
