edc4652fb9fc0bbf82241ade016559cb0d8a8f85
Sabay <asabay@gmail.com>
2024-11-03 21:24:56 +0300

    -

--- Classes/Controller/ConfigController.php
+++ Classes/Controller/ConfigController.php
@@ -36,9 +36,9 @@ class ConfigController extends ActionController
         $myConfiguration = $this->extensionConfiguration->get('clickio_consent');
         $moduleTemplate = $this->moduleTemplateFactory->create($this->request);
 
-        $moduleTemplate->assignMultiple([
-            $myConfiguration
-        ]);
+        $moduleTemplate->assignMultiple($myConfiguration);
+        $moduleTemplate->assignMultiple('options' => print_r($myConfiguration,true));
+
         return $moduleTemplate->renderResponse('Config/Index');
 
         // return $this->htmlResponse();
