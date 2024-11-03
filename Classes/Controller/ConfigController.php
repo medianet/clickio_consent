0baf62c1c29922470dc53b8451c8657bf02b7695
Sabay <asabay@gmail.com>
2024-11-03 20:09:52 +0300

    -

--- Classes/Controller/ConfigController.php
+++ Classes/Controller/ConfigController.php
@@ -4,8 +4,20 @@ namespace Clickio\ClickioConsent\Controller;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use TYPO3\CMS\Core\Http\Response;
 
+use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
+
+
 class ConfigController extends ActionController
 {
+
+
+    /** @var ModuleTemplateFactory */
+    protected $moduleTemplateFactory;
+
+    public function injectModuleTemplateFactory(ModuleTemplateFactory $moduleTemplateFactory)
+    {
+        $this->moduleTemplateFactory = $moduleTemplateFactory;
+    }
 
     public function indexAction(): Response
     {
@@ -13,6 +25,10 @@ class ConfigController extends ActionController
         $this->view->assignMultiple([
             'counts' => 1,
         ]);
-        return $this->htmlResponse();
+
+        $moduleTemplate = $this->moduleTemplateFactory->create($this->request);
+        return $moduleTemplate->renderResponse('Config/Index');
+
+        // return $this->htmlResponse();
     }
 }
