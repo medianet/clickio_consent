ec73c97b7dcc684eaec5ab7f20e48e847cc7e82a
Sabay <asabay@gmail.com>
2024-11-03 20:24:39 +0300

    -

--- Classes/Controller/ConfigController.php
+++ Classes/Controller/ConfigController.php
@@ -5,6 +5,7 @@ use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use TYPO3\CMS\Core\Http\Response;
 
 use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 
 
 class ConfigController extends ActionController
@@ -14,18 +15,28 @@ class ConfigController extends ActionController
     /** @var ModuleTemplateFactory */
     protected $moduleTemplateFactory;
 
+    /** @var ExtensionConfiguration */
+    protected $extensionConfiguration
+
     public function injectModuleTemplateFactory(ModuleTemplateFactory $moduleTemplateFactory)
     {
         $this->moduleTemplateFactory = $moduleTemplateFactory;
     }
 
+    public function injectExtensionConfiguration(ExtensionConfiguration $extensionConfiguration)
+    {
+        $this->extensionConfiguration = $extensionConfiguration;
+    }
+
     public function indexAction(): Response
     {
+        $myConfiguration = $this->extensionConfiguration
+            ->get('clickio_consent');
+
 
         $this->view->assignMultiple([
-            'counts' => 1,
+            'options' => $myConfiguration,
         ]);
-
         $moduleTemplate = $this->moduleTemplateFactory->create($this->request);
         return $moduleTemplate->renderResponse('Config/Index');
 
