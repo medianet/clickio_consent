d38dcb058b5f40f2c2af82a65c1f2f4da3b942ca
Sabay <asabay@gmail.com>
2024-11-04 18:05:48 +0300

    -

---
+++ Classes/Hooks/PageRenderer/ConsentCodeInjector.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Clickio\ClickioConsent\Hooks\PageRenderer;
+
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Page\PageRenderer;
+use TYPO3\CMS\Core\Site\Entity\Site;
+
+/**
+ * @internal
+ */
+final class TrackingCodeInjector
+{
+    public function __construct(
+        private readonly EventDispatcherInterface $eventDispatcher,
+    ) {}
+
+    /**
+     * @param array{}|null $params
+     */
+    public function execute(?array &$params, PageRenderer $pageRenderer): void
+    {
+        $request = $this->getRequest();
+        if ($request->getAttribute('applicationType') !== 1) {
+            // Not a frontend request
+            return;
+        }
+        $pageRenderer->addHeaderData('<script> alert('11111'); </script>');
+
+    }
+
+/*
+    private function hasValidConfiguration(Configuration $configuration): bool
+    {
+        if (! \filter_var($configuration->url, \FILTER_VALIDATE_URL)) {
+            return false;
+        }
+
+        return $configuration->siteId > 0;
+    }
+*/
+
+    private function getRequest(): ServerRequestInterface
+    {
+        return $GLOBALS['TYPO3_REQUEST'];
+    }
+}
