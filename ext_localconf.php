31842bb0ccc7ebb064cb1dbd41cf37fb1ef9e490
Sabay <asabay@gmail.com>
2024-11-04 18:12:54 +0300

    -

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,4 +3,5 @@
 defined('TYPO3') or die('Access denied.');
 
 // Add default RTE configuration
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['clickio_consent'] = 'EXT:clickio_consent/Configuration/RTE/Default.yaml';
+//$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['clickio_consent'] = 'EXT:clickio_consent/Configuration/RTE/Default.yaml';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-postTransform'][] = ConsentCodeInjector::class . '->execute';
