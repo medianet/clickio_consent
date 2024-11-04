d1bb193e05264f445248e0cb65d8190ea07a4d11
Sabay <asabay@gmail.com>
2024-11-04 17:24:54 +0300

    -

--- Resources/Public/JavaScript/main.js
+++ Resources/Public/JavaScript/main.js
@@ -1,16 +1,18 @@
 console.log('main ready');
-
-function hintShow(id){
-    const ob = document.getElementById(id+'_hint');
-    if( ob.style.display !== "block" ){
-         ob.style.display="block";
-    } else {
-         ob.style.display="none";
-    }
-}
-
-
 document.addEventListener('DOMContentLoaded', function () {
+
+           const els = Array.from(document.querySelectorAll('[data-show]'));
+           for(const el of els) {
+             el.addEventListener('change', () => {
+               const id = el.getAttribute("data-show")
+               const ob = document.getElementById(id +'_hint');
+               if( ob.style.display !== "block" ){
+                    ob.style.display="block";
+               } else {
+                    ob.style.display="none";
+               }
+             });
+           }
            const consent_enabled = document.getElementById('consent_enabled');
            function toggleAdditionalField() {
                const consent_enabled_section = document.getElementById('consent_enabled_section');
